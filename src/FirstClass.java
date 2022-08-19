ccb18a16b2258bfbf3058b582c2aa558f411385c
eshingji <eshji123@gmail.com>
2022-08-19 10:30:28 -0500

    first commit

---
+++ src/FirstClass.java
@@ -0,0 +1,21 @@
+public class FirstClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("How are you today?\nI hope you're nifty.");
+
+        int numDwarves; // instantiation
+        numDwarves = 7; //initialization
+
+        int numStooges = 3; //instantiation & initialization together
+        System.out.println("There are "+numDwarves+" dwarves in my cottage.");
+        System.out.println("At first, there were "+numStooges+" stooges.");
+
+        numStooges = numStooges + 1;
+//        numStooges += 1;
+//        numStooges++;
+
+        System.out.println("Now, there are "+numStooges+" stooges.");
+    }
+}
